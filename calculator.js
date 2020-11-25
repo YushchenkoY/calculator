340851ba40c8a30f375dbae9078a1b9a104aadaf
yushchenko yuliia <yushchenko.yu@gmail.com>
2020-11-25 20:21:28 +0200

    fixed C and +/-

--- calculator.js
+++ calculator.js
@@ -1,19 +1,28 @@
 const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.calculator__display');
+
 buttons.forEach(function(button) {
+
     button.addEventListener('click', calculate);
+    display.value = '';
+
 });
-// document.querySelector('.calculator__display').value = 0;
+
+document.querySelector('.calculator__display').value = 0;
+
 function calculate(event) {
 
     const clickedButtonValue = event.target.value;
+
 
     if (clickedButtonValue === '=') {
         if (display.value !== '') {
             display.value = eval(display.value);
         }
-    // } else if (clickedButtonValue === 'C') {
-    //     display.value = '';
+    } else if (clickedButtonValue === "С") {
+        display.value = '0';
+    } else if (clickedButtonValue === "+/-") {
+        display.value = display.value*-1
     } else {
         display.value += clickedButtonValue;
     }
